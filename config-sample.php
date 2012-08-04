dc74d867b9075da3a88563897946199caef696a1
David Wilkinson <davidw@lumen.co.uk>
2012-08-04 15:40:08 +0100

    Remove unneeded variable from sample config file

--- config-sample.php
+++ config-sample.php
@@ -6,6 +6,3 @@
 $flickrApiKey = 'PUT_YOUR_FLICKR_API_KEY_HERE';
 $flickrApiSecret = 'PUT_YOUR_FLICKR_API_SECRET_HERE';
 
-
-// For the authentication example, put the URL of your web site here, together with the path to the DPZFlickr library
-$flickrCallbackBaseUrl = 'http://www.mywebsite.com/path/to/DPZFlickr/';
